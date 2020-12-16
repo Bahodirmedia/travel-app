95cf91c20b2b50c4d25502740b87bf5094df5fd5
Bahodir Abdullayev <60607484+Bahodirmedia@users.noreply.github.com>
2020-12-16 13:08:32 +0500

    Show undefined

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -16,12 +16,12 @@ const GEONAMES_USERNAME = 'bahodir_media';
 const PIXABAY_API_KEY = '19509353-9c8b90d5b1914625113889dbf';
 
 let handleSubmit = (e) => {
+	e.preventDefault()
+	// Information from user inputs
 	let fromCity = document.querySelector('#from').value;
 	let toCity = document.querySelector('#to').value;
 	let when = document.querySelector('#date').value;
 	const timeNow = (Date.now()) / 1000;
-	e.preventDefault()
-	// Information from user inputs
 	const time = (new Date(when).getTime()) / 1000;
 
 	//Validating inputs
@@ -51,11 +51,11 @@ let handleSubmit = (e) => {
 				});
 				return allData
 			})
-			.then((allData) => {
-				console.log(allData);
-				const cityImg = cityImg(allData.toCity)
-				return cityImg;
-			})
+			// .then((allData) => {
+			// 	console.log(allData);
+			// 	const cityImg = cityImg(allData.toCity)
+			// 	return cityImg;
+			// })
 			.then((allData) => {
 				show(allData);
 			})
